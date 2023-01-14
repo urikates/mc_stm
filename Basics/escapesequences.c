4c9b1f30b6bfda15655dbd0ddc76798d3030e94d
urikates <uvr3141@gmail.com>
2023-01-13 18:59:24 -0600

    Update

---
+++ Basics/escapesequences.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("David says, \"Programming is fun!\"\n");
+    printf("Good day!");
+    return 0;
+}
---
+++ Basics/escapesequencesHW.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("David says \"Programming is fun!\"\n");
+    printf("**Conditions apply, \"Offers valid until tomorrow\"\n");
+    printf("C: \\My_computer\\My_folder\n");
+    printf("D:/My_documents/My_file\n");
+    printf("\\ \\ \\ \\ Today is holiday \\ \\ \\\n");
+    printf("This is a triple quoted string \"\"\" This month has 30 days \"\"\"\n");
+    return 0;
+}
